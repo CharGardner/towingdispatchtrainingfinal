2a7c97d83825a46d823dbde153928abc1deb670e
CharGardner <char.j.gardner@gmail.com>
2024-10-28 15:00:38 -0500

    Add files via upload

---
+++ story_content/6fkz8GdXFsO_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.92.33293.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.709%0D%0AThank%20you%20for%20calling%20company%20name.%20How%20can%20we%20help%20with%20towing%20today?%0D%0A%0D%0A"}')
